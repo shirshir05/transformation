3ad2330fcdc884a9790be834da3d03be0204cb58
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-07-02 16:52:45 +0300

    before

---
+++ transforms/VariableRenaming/0b3ea918139583bebec3240194ddbd41b987d486_after_SASLAuthenticationProvider_4798_new_.java
@@ -0,0 +1,55 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.zookeeper.server.auth;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.server.ServerCnxn;
+
+public class SASLAuthenticationProvider implements AuthenticationProvider {
+
+    public String getScheme() {
+        return "sasl";
+    }
+
+    public KeeperException.Code handleAuthentication(ServerCnxn cnxn, byte[] authData) {
+        // a call to this method (SASLAuthenticationProvider:handleAuthentication()) at session initiation.
+        return KeeperException.Code.AUTHFAILED;
+    }
+
+    public boolean matches(String id, String aclExpr) {
+        if (System.getProperty("zookeeper.superUser") != null) {
+            return (id.equals(System.getProperty("zookeeper.superUser")) || id.equals(aclExpr));
+        }
+        return (id.equals("super") || id.equals(aclExpr));
+    }
+
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    public boolean isValid(String id) {
+        // 
+        try {
+            new KerberosName(id);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
+
