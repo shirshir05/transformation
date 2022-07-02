67257a3e31be5f65da8d6895a5ce3e442e868fc6
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-07-02 18:12:04 +0300

    \VariableRenaming\

--- transforms/VariableRenaming/30cff66e5efe6b99b7e7a2293061f0eea0def02f_after_FinalRequestProcessor_9199_new_.java
+++ transforms/VariableRenaming/30cff66e5efe6b99b7e7a2293061f0eea0def02f_after_FinalRequestProcessor_9199_new_.java
@@ -47,8 +47,6 @@ import org.apache.zookeeper.proto.SetWatches;
 import org.apache.zookeeper.proto.SyncRequest;
 import org.apache.zookeeper.proto.SyncResponse;
 import org.apache.zookeeper.server.DataTree.ProcessTxnResult;
-import org.apache.zookeeper.server.NIOServerCnxn.CnxnStats;
-import org.apache.zookeeper.server.NIOServerCnxn.Factory;
 import org.apache.zookeeper.server.ZooKeeperServer.ChangeRecord;
 import org.apache.zookeeper.txn.CreateSessionTxn;
 import org.apache.zookeeper.txn.ErrorTxn;
@@ -114,7 +112,7 @@ public class FinalRequestProcessor implements RequestProcessor {
             }
         }
         if (request.hdr != null && request.hdr.getType() == OpCode.closeSession) {
-            Factory scxn = zks.getServerCnxnFactory();
+            ServerCnxnFactory scxn = zks.getServerCnxnFactory();
             // we might just be playing diffs from the leader
             if (scxn != null && request.cnxn == null) {
                 // in the switch block below
@@ -147,7 +145,7 @@ public class FinalRequestProcessor implements RequestProcessor {
                     {
                         zks.serverStats().updateLatency(request.createTime);
                         lastOp = "PING";
-                        ((CnxnStats) cnxn.getStats()).updateForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
+                        cnxn.updateStatsForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
                         cnxn.sendResponse(new ReplyHeader(-2, zks.getZKDatabase().getDataTreeLastProcessedZxid(), 0), null, "response");
                         return;
                     }
@@ -155,8 +153,8 @@ public class FinalRequestProcessor implements RequestProcessor {
                     {
                         zks.serverStats().updateLatency(request.createTime);
                         lastOp = "SESS";
-                        ((CnxnStats) cnxn.getStats()).updateForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
-                        cnxn.finishSessionInit(true);
+                        cnxn.updateStatsForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
+                        zks.finishSessionInit(request.cnxn, true);
                         return;
                     }
                 case OpCode.create:
@@ -226,7 +224,7 @@ public class FinalRequestProcessor implements RequestProcessor {
                         }
                         Long aclL;
                         synchronized (n) {
-                            aclL = n.acl;
+                            aclL = n.var25;
                         }
                         PrepRequestProcessor.checkACL(zks, zks.getZKDatabase().convertLong(aclL), ZooDefs.Perms.READ, request.authInfo);
                         Stat stat = new Stat();
@@ -251,8 +249,8 @@ public class FinalRequestProcessor implements RequestProcessor {
                         GetACLRequest getACLRequest = new GetACLRequest();
                         ZooKeeperServer.byteBuffer2Record(request.request, getACLRequest);
                         Stat stat = new Stat();
-                        List<ACL> acl = zks.getZKDatabase().getACL(getACLRequest.getPath(), stat);
-                        rsp = new GetACLResponse(acl, stat);
+                        List<ACL> var25 = zks.getZKDatabase().getACL(getACLRequest.getPath(), stat);
+                        rsp = new GetACLResponse(var25, stat);
                         break;
                     }
                 case OpCode.getChildren:
@@ -266,7 +264,7 @@ public class FinalRequestProcessor implements RequestProcessor {
                         }
                         Long aclG;
                         synchronized (n) {
-                            aclG = n.acl;
+                            aclG = n.var25;
                         }
                         PrepRequestProcessor.checkACL(zks, zks.getZKDatabase().convertLong(aclG), ZooDefs.Perms.READ, request.authInfo);
                         List<String> children = zks.getZKDatabase().getChildren(getChildrenRequest.getPath(), null, getChildrenRequest.getWatch() ? cnxn : null);
@@ -285,7 +283,7 @@ public class FinalRequestProcessor implements RequestProcessor {
                         }
                         Long aclG;
                         synchronized (n) {
-                            aclG = n.acl;
+                            aclG = n.var25;
                         }
                         PrepRequestProcessor.checkACL(zks, zks.getZKDatabase().convertLong(aclG), ZooDefs.Perms.READ, request.authInfo);
                         List<String> children = zks.getZKDatabase().getChildren(getChildren2Request.getPath(), stat, getChildren2Request.getWatch() ? cnxn : null);
@@ -313,7 +311,7 @@ public class FinalRequestProcessor implements RequestProcessor {
         }
         ReplyHeader hdr = new ReplyHeader(request.cxid, request.zxid, err.intValue());
         zks.serverStats().updateLatency(request.createTime);
-        ((CnxnStats) cnxn.getStats()).updateForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
+        cnxn.updateStatsForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
         try {
             cnxn.sendResponse(hdr, rsp, "response");
             if (closeSession) {
@@ -329,4 +327,3 @@ public class FinalRequestProcessor implements RequestProcessor {
         LOG.info("shutdown of request processor complete");
     }
 }
-
