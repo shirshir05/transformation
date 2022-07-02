ab5f7d78ef392d6a04d83f5037495f52ccfad751
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-07-02 18:12:39 +0300

    \ReorderCondition\

--- transforms/ReorderCondition/30cff66e5efe6b99b7e7a2293061f0eea0def02f_after_FinalRequestProcessor_9232_new_.java
+++ transforms/ReorderCondition/30cff66e5efe6b99b7e7a2293061f0eea0def02f_after_FinalRequestProcessor_9232_new_.java
@@ -47,8 +47,6 @@ import org.apache.zookeeper.proto.SetWatches;
 import org.apache.zookeeper.proto.SyncRequest;
 import org.apache.zookeeper.proto.SyncResponse;
 import org.apache.zookeeper.server.DataTree.ProcessTxnResult;
-import org.apache.zookeeper.server.NIOServerCnxn.CnxnStats;
-import org.apache.zookeeper.server.NIOServerCnxn.Factory;
 import org.apache.zookeeper.server.ZooKeeperServer.ChangeRecord;
 import org.apache.zookeeper.txn.CreateSessionTxn;
 import org.apache.zookeeper.txn.ErrorTxn;
@@ -97,7 +95,7 @@ public class FinalRequestProcessor implements RequestProcessor {
             }
             if (request.hdr != null) {
                 rc = zks.getZKDatabase().processTxn(request.hdr, request.txn);
-                if (request.type == OpCode.createSession) {
+                if (OpCode.createSession == request.type) {
                     if (request.txn instanceof CreateSessionTxn) {
                         CreateSessionTxn cst = (CreateSessionTxn) request.txn;
                         zks.sessionTracker.addSession(request.sessionId, cst.getTimeOut());
@@ -114,7 +112,7 @@ public class FinalRequestProcessor implements RequestProcessor {
             }
         }
         if (request.hdr != null && request.hdr.getType() == OpCode.closeSession) {
-            Factory scxn = zks.getServerCnxnFactory();
+            ServerCnxnFactory scxn = zks.getServerCnxnFactory();
             // we might just be playing diffs from the leader
             if (scxn != null && request.cnxn == null) {
                 // in the switch block below
@@ -147,7 +145,7 @@ public class FinalRequestProcessor implements RequestProcessor {
                     {
                         zks.serverStats().updateLatency(request.createTime);
                         lastOp = "PING";
-                        ((CnxnStats) cnxn.getStats()).updateForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
+                        cnxn.updateStatsForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
                         cnxn.sendResponse(new ReplyHeader(-2, zks.getZKDatabase().getDataTreeLastProcessedZxid(), 0), null, "response");
                         return;
                     }
@@ -155,8 +153,8 @@ public class FinalRequestProcessor implements RequestProcessor {
                     {
                         zks.serverStats().updateLatency(request.createTime);
                         lastOp = "SESS";
-                        ((CnxnStats) cnxn.getStats()).updateForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
-                        cnxn.finishSessionInit(true);
+                        cnxn.updateStatsForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
+                        zks.finishSessionInit(request.cnxn, true);
                         return;
                     }
                 case OpCode.create:
@@ -313,7 +311,7 @@ public class FinalRequestProcessor implements RequestProcessor {
         }
         ReplyHeader hdr = new ReplyHeader(request.cxid, request.zxid, err.intValue());
         zks.serverStats().updateLatency(request.createTime);
-        ((CnxnStats) cnxn.getStats()).updateForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
+        cnxn.updateStatsForResponse(request.cxid, request.zxid, lastOp, request.createTime, System.currentTimeMillis());
         try {
             cnxn.sendResponse(hdr, rsp, "response");
             if (closeSession) {
@@ -329,4 +327,3 @@ public class FinalRequestProcessor implements RequestProcessor {
         LOG.info("shutdown of request processor complete");
     }
 }
-
