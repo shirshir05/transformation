50da7e840d517059ff8f471b1a428dcc1791e180
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-07-02 16:44:07 +0300

    \ReorderCondition\

--- transforms/ReorderCondition/0a4971d5efb12a35a00cf794dce1efb3f08a998c_after_ContainerManager_4318_new_.java
+++ transforms/ReorderCondition/0a4971d5efb12a35a00cf794dce1efb3f08a998c_after_ContainerManager_4318_new_.java
@@ -148,7 +148,24 @@ public class ContainerManager {
                 candidates.add(containerPath);
             }
         }
+        for (String ttlPath : zkDb.getDataTree().getTtls()) {
+            DataNode node = zkDb.getDataTree().getNode(ttlPath);
+            if (node != null) {
+                Set<String> children = node.getChildren();
+                if ((children == null) || (children.size() == 0)) {
+                    long elapsed = getElapsed(node);
+                    long ttl = EphemeralType.getTTL(node.stat.getEphemeralOwner());
+                    if ((ttl != 0) && (ttl < elapsed)) {
+                        candidates.add(ttlPath);
+                    }
+                }
+            }
+        }
         return candidates;
     }
-}
 
+    // VisibleForTesting
+    protected long getElapsed(DataNode node) {
+        return Time.currentWallTime() - node.stat.getMtime();
+    }
+}
