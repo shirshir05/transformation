f1fe29466215a5991396aa3dd957b6cb50d48875
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-07-02 17:49:48 +0300

    \ReorderCondition\

--- transforms/ReorderCondition/297517b5e1461400039aa7c4fdbbd246183a133f_after_ReadOnlyRequestProcessor_7961_new_.java
+++ transforms/ReorderCondition/297517b5e1461400039aa7c4fdbbd246183a133f_after_ReadOnlyRequestProcessor_7961_new_.java
@@ -26,6 +26,8 @@ import org.apache.zookeeper.server.Request;
 import org.apache.zookeeper.server.RequestProcessor;
 import org.apache.zookeeper.server.ZooKeeperServer;
 import org.apache.zookeeper.server.ZooTrace;
+import org.apache.zookeeper.server.RequestProcessor.RequestProcessorException;
+import org.apache.zookeeper.server.quorum.Leader.XidRolloverException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -86,12 +88,19 @@ public class ReadOnlyRequestProcessor extends Thread implements RequestProcessor
                         continue;
                 }
                 // proceed to the next processor
-                if (nextProcessor != null) {
+                if (null != nextProcessor) {
                     nextProcessor.processRequest(request);
                 }
             }
         } catch (InterruptedException e) {
             LOG.error("Unexpected interruption", e);
+        } catch (RequestProcessorException e) {
+            if (e.getCause() instanceof XidRolloverException) {
+                LOG.info(e.getCause().getMessage());
+            }
+            LOG.error("Unexpected exception", e);
+        } catch (Exception e) {
+            LOG.error("Unexpected exception", e);
         }
         LOG.info("ReadOnlyRequestProcessor exited loop!");
     }
@@ -111,4 +120,3 @@ public class ReadOnlyRequestProcessor extends Thread implements RequestProcessor
         nextProcessor.shutdown();
     }
 }
-
